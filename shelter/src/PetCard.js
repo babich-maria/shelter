af55fd106f6290db0cc8efd884bc62cf933d3e43
“babich-maria” <Ljurka@gmail.com>
2024-09-09 00:12:44 +0200

    feat: modal window+burger

--- shelter/src/PetCard.js
+++ shelter/src/PetCard.js
@@ -1,3 +1,26 @@
-export class PetCard{
-  
+export class PetCard {
+  constructor({ id, img, name }) {
+    this.id = id;
+    this.urlToImage = img;
+    this.name = name;
+  }
+
+  generatePetCard() {
+    let template = '';
+    let pet = document.createElement('div');
+    pet.className = 'section-pets__card';
+    pet.setAttribute('data-id', this.id);
+
+    this.urlToImage &&
+      (template += `<img class="pet__image" src=${this.urlToImage} alt="pet">`);
+
+    this.name &&
+      (template += `<p class="section-pets__name">${this.name}</p>`);
+
+    template += ` <button class="button_bodered">Learn more</button>`
+    template += `</div>`;
+
+    pet.innerHTML = template;
+    return pet;
+  }
 }
--- shelter/webpack.config.js
+++ shelter/webpack.config.js
@@ -1,5 +1,23 @@
-entry: ['./src/index.js', 'style.scss']
+
+
+const path = require('path');
 
 module.exports = {
   mode: 'development',
+  entry: {
+    main: './src/index.js', // точка входа для index.html
+    pets: './src/our_pets.js'   // точка входа для pets.html
+  },
+  output: {
+    filename: '[name].js', // использование [name] для генерации отдельных файлов
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'] // загрузчики для работы со SCSS
+      }
+    ]
+  },
 };
